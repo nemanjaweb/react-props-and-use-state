828de2955de27cfd854fc9ca001c9b7391ef6705
abc <abc@example.com>
2022-04-13 12:47:28 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
+      <h1>My Todo List</h1>
       <Todo text="Learn React" />
       <Todo text="Master React" />
       <Todo text="Get job as React developer" />
