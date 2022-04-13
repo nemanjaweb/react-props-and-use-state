fca93f5b0194a2f0efd33cdcbf5e8a6add41ed26
abc <abc@example.com>
2022-04-13 12:24:45 +0200

    props

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todo = (props) => {
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
